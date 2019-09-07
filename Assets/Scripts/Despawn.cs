0f9fd243af8f2bebb40f72e07e59f30659a52c1f
MULTIUSER-Devloper <molundchristian@gmail.com>
2019-09-08 00:16:32 +0200

    nothing really
    k

---
+++ Assets/Scripts/Despawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour
+{ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Object.Destroy(this, 2.0f);
+    }
+}
