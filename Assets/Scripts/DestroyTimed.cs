db4485682aa28992b2cdc13219153c24a9a82f7b
MULTIUSER-Devloper <molundchristian@gmail.com>
2019-09-07 23:48:40 +0200

    Bullet Prefab
    yeh

---
+++ Assets/Scripts/DestroyTimed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimed : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewMethod();
+    }
+    private void NewMethod()
+    {
+        Destroy(this, t: 5); //5 is how many seconds you want before the object deletes itself
+    }
+}
